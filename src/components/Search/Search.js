f1b8b07e73006604d4ef3ae0cf695a715f5cf392
Tina Safari <tina.safari85@gmail.com>
2020-06-17 12:08:08 -0400

    add header & fixed the Routein theApp

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,42 +1,34 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
 import Book from "../Book/Book";
 
 class Search extends Component{
-    static propTypes = {
-        Books: PropTypes.array.isRequired,
-        shelfStatus: PropTypes.func.isRequired,
-    }
-    state = {
-        query: ''
-    }
-    updateQuery = (query) => {
-        this.setState(() => ({
-            query: query.trim()
-        }))
-    }
-    clearQuery = () => {
-        this.updateQuery('')
-    }
-    render() {
-        const { query } = this.state
-        const { Books, shelfStatus } = this.props
 
-        const showSearchPage = query === ''
-            ? Books
-            : Books.filter((c) => (
-                c.name.toLowerCase().includes(query.toLowerCase())
-            ))
+    // state = {
+    //     query: ''
+    // }
+    // updateQuery = (query) => {
+    //     this.setState(() => ({
+    //         query: query.trim()
+    //     }))
+    // }
+    // clearQuery = () => {
+    //     this.updateQuery('')
+    // }
+    render() {
+    //     const { query } = this.state
+    //     const { Books, shelfStatus } = this.props
+    //
+    //     const showSearchPage = query === ''
+    //         ? Books
+    //         : Books.filter((c) => (
+    //             c.name.toLowerCase().includes(query.toLowerCase())
+    //         ))
 
         console.log(this.props.searchlist)
         return(
             <div>
-                <Book bookProp={this.props.searchlist}/>
-                <Link
-                    to='/search'
-                    className='open-search'
-                >Add a book</Link>
+
             </div>
 
         // {this.props.showSearchPage ? (
